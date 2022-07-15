3bd1f6ce8193c3e259f16fe1c876f2305c46de59
mektigboy <mektigboy@protonmail.com>
2022-07-15 14:58:02 -0500

    Update files

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const element = document.getElementById("root");
+const root = ReactDOM.createRoot(element);
+
+class App extends React.Component {}
+
+root.render(<App />);
