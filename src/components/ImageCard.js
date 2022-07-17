04ebd9bfc137b82377b29f95df531530a8dc9ade
mektigboy <mektigboy@protonmail.com>
2022-07-17 17:38:48 -0500

    Update files

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -3,17 +3,25 @@ import React from "react";
 class ImageCard extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { spans: 0 };
     this.imageReference = React.createRef();
   }
 
   componentDidMount() {
-    console.log(this.imageReference.current.clientHeight);
+    this.imageReference.current.addEventListener("load", this.setSpans);
   }
+
+  setSpans = () => {
+    const height = console.log(this.imageReference.current.clientHeight);
+    const spans = Math.ceil(height / 150);
+
+    this.setState({ spans });
+  };
 
   render() {
     const { description, urls } = this.props.image;
     return (
-      <div>
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
         <img ref={this.imageReference} alt={description} src={urls.regular} />
       </div>
     );
